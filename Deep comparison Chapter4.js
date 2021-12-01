0b96c2e26d33eea6ed0e950d6d31ff3cf401b06b
damian94oka <67159438+web-developer-damian94oka@users.noreply.github.com>
2021-12-01 20:26:17 +0100

    Update Deep comparison Chapter4.js

--- Deep comparison Chapter4.js
+++ Deep comparison Chapter4.js
@@ -27,3 +27,11 @@ function deepEqual (x, y) {
     }
     return true;
   }
+
+/*let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true*/
